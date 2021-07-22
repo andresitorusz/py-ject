3441fa0a8d3dfdec69f1d9b8638e998ed79cef78
Andre Yehuda Sitorus <andresitorus@bitbucket.org>
2021-07-22 16:57:17 +0700

    Add main program and pays method

--- app-2-flatmates-bill/flatmate.py
+++ app-2-flatmates-bill/flatmate.py
@@ -8,5 +8,7 @@ class Flatmate:
         self.name = name
         self.days_in_apart = days_in_apart
 
-    def pays(self, bill):
-        pass
+    def pays(self, bill, co_flatmate):
+        weight = self.days_in_apart / (self.days_in_apart + co_flatmate.days_in_apart)
+        bill = bill.amount * weight
+        return bill
---
+++ app-2-flatmates-bill/main.py
@@ -0,0 +1,34 @@
+from colorama import Fore
+
+from bill import Bill
+from flatmate import Flatmate
+
+
+def main():
+    print(Fore.GREEN + "Hello there! Welcome to flatmates bill counter application. \nPlease fill the form below, "
+                       "so we can count your and your flatmate bill.")
+    amount, period, flatmate_name, co_flatmate_name, flatmate_days_in_apart, co_flatmate_days_in_apart = fill_the_form()
+
+    bill = Bill(amount=amount, period=period)
+    flatmate = Flatmate(name=flatmate_name, days_in_apart=flatmate_days_in_apart)
+    co_flatmate = Flatmate(name=co_flatmate_name, days_in_apart=co_flatmate_days_in_apart)
+
+    print(f"{flatmate.name} pays: ${flatmate.pays(bill, co_flatmate)}")
+    print(f"{co_flatmate.name} pays: ${co_flatmate.pays(bill, flatmate)}")
+
+
+def fill_the_form():
+    flatmate_name = input(Fore.RESET + "What is your name? ")
+    amount = float(input(f"Hi {flatmate_name}, enter the total bill amount: $"))
+    period = input("What is the bill period? (e.g. July 2021): ")
+    co_flatmate_name = input("What is the name of the other flatmate? ")
+    flatmate_days_in_apart = int(input("How many days did you stay in the house during the bill period? "))
+    co_flatmate_days_in_apart = int(input(
+        f"How many days did {co_flatmate_name} stay in the apart during the bill period? "
+    ))
+
+    return amount, period, flatmate_name, co_flatmate_name, flatmate_days_in_apart, co_flatmate_days_in_apart
+
+
+if __name__ == '__main__':
+    main()
