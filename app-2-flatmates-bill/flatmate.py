70bdf589f52145f22b0f19e4667ad82431389383
Andre Yehuda Sitorus <andresitorus@bitbucket.org>
2021-07-22 12:43:28 +0700

    Initial commit

---
+++ app-2-flatmates-bill/flatmate.py
@@ -0,0 +1,8 @@
+class Flatmate:
+
+    def __init__(self, name, days_in_apart):
+        self.name = name
+        self.days_in_apart = days_in_apart
+
+    def pays(self, bill):
+        pass
---
+++ app-2-flatmates-bill/pdf_report.py
@@ -0,0 +1,7 @@
+class PdfReport:
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        pass
